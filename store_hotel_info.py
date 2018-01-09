dc24b2bec12659ca9f6962ffac925d4a023ab826
Ayan Das <ayandas@lawn-143-215-60-238.lawn.gatech.edu>
2018-01-08 22:24:51 -0500

    Added a Jupyter notebook for doing proper analysis of the reviews

--- store_hotel_info.py
+++ store_hotel_info.py
@@ -14,28 +14,29 @@ processedHotels = {}
 # Get the addresses and cities for all of the hotels
 for review in reviews:
     if review['hotel_name'] not in processedHotels:
-        processedHotels[review['hotel_name']] = { "address": review["hotel_address"], "city": review["city"], "latitude": review["hotel_latitude"], "longitude": review["hotel_longitude"] }
+        processedHotels[review['hotel_name']] = { "hotel_address": review["hotel_address"], "city": review["city"], "latitude": review["hotel_latitude"], "longitude": review["hotel_longitude"] }
 
 print("Got all hotels")
 
 # Get average and standard deviation of scores for each hotel_latitude
 i = 1
 
-fieldNames = ["name", "address", "city", "latitude", "longitude", "review_count", "average_score", "score_standard_deviation", "skew"]
+fieldNames = ["name", "hotel_address", "city", "latitude", "longitude"]
 outputFile = open('hotel_info.csv', 'w')
 writer = csv.DictWriter(outputFile, fieldnames=fieldNames)
 writer.writeheader()
 
 for hotel in processedHotels:
     print(str(i) + ". " + hotel)
-    relevantReviews = filter(lambda x: x["hotel_name"] == hotel, reviews)
+    # These stats will be addressed in a Jupyter notebook with Pandas code
+    """relevantReviews = filter(lambda x: x["hotel_name"] == hotel, reviews)
     hotelScores = np.array(map(lambda x: float(x["score"]), relevantReviews))
     averageScore = hotelScores.mean()
-    standardDeviation = hotelScores.std()
+    standardDeviation = hotelScores.std()"""
     processedHotels[hotel]["name"] = hotel
-    processedHotels[hotel]["average_score"] = averageScore
+    """processedHotels[hotel]["average_score"] = averageScore
     processedHotels[hotel]["score_standard_deviation"] = standardDeviation
     processedHotels[hotel]["review_count"] = hotelScores.size
-    processedHotels[hotel]["skew"] = skew(hotelScores)
+    processedHotels[hotel]["skew"] = skew(hotelScores)"""
     writer.writerow(processedHotels[hotel])
     i += 1
